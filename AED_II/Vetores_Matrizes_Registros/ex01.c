60f8911e809a670bc8ec2dd8cb215e40f01d99c2
Vinicius <113783249+Vinicius203@users.noreply.github.com>
2023-02-15 11:26:38 -0300

    Update ex01.c

--- AED_II/Vetores_Matrizes_Registros/ex01.c
+++ AED_II/Vetores_Matrizes_Registros/ex01.c
@@ -1,26 +1,51 @@
 #include <stdio.h>
+#include <stdlib.h>
 
-void ordenaVetor(int v[], int tam);
+void imprimeVetor(int *v, int tam);
+void preencheVetor(int *v, int tam);
+void retornaMaiorMenor(int *v, int tam, int *menor, int *maior);
 
 int main()
 {
-    int v[10] = {2, 4, 6, 8, 10, 12, 14, 16, 18, 20};
-    ordenaVetor(v, 10);
+    int tam, maior = 0, menor = 1000;
+
+    printf("Digite o tamanho do seu vetor:\n");
+    scanf("%d", &tam);
+
+    int v[tam];
+
+    srand(time(NULL));
+
+    preencheVetor(v, tam);
+    imprimeVetor(v, tam);
+    retornaMaiorMenor(v, tam, &menor, &maior);
+    printf("O maior num e: %d\nO menor num e: %d\n\n", maior, menor);
     return 0;
 }
 
-void ordenaVetor(int v[], int tam)
-{
-    int maior, menor;
-    maior = menor = v[0];
+void preencheVetor(int *v, int tam) {
+    for (int i = 0; i < tam; i ++) {
+        *(v+i) = rand() % 100;
+    }
+}
+
+void imprimeVetor(int *v, int tam) {
+    for(int i = 0; i < tam; i++) {
+        printf("%d\t", *(v+i));
+    }
+    printf("\n");
+}
+
+void retornaMaiorMenor(int *v, int tam, int *menor, int *maior) {
     for(int i = 0; i < tam; i ++) {
-        if(v[i] > maior) {
-            maior = v[i];
+        if(*(v+i) < *menor) {
+            *menor = *(v+i);
         }
-        if(v[i] < menor) {
-            menor = v[i];
+        if(*(v+i) > *maior) {
+            *maior = *(v+i);
         }
     }
-    printf("O menor valor e: %d\n", menor);
-    printf("O maior valor e: %d\n", maior);
 }
+
+
+
